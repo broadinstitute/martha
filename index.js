5b51ff56982f38f6cf4505f672b20fae01e92880
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2018-03-12 10:49:49 -0400

    one more tweak for path

--- index.js
+++ index.js
@@ -9,12 +9,17 @@ const url = require('url')
 exports.martha_v1 = (req, res) => {
     var orig_url = req.body.url;
     var parsed_url = url.parse(orig_url);
+    var orig_path = parsed_url.path;
+    var new_path = '/api/ga4gh/dos/v1/dataobjects' + orig_path;
     parsed_url.protocol = 'https';
+    parsed_url.path = new_path;
+    parsed_url.pathname = new_path;
     var http_url = url.format(parsed_url);
+    console.log(http_url);
     superagent.get(http_url)
         .end(function(err, response) {
             if(err){
-                console.error(err);
+                // console.error(err);
                 res.status(502).send(err);
                 return;
             };
