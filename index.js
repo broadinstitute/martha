7d56d03bd5ef41a84ff3075ecb40e7fc79ebb69c
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2018-03-19 13:25:08 -0400

    making prettier

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const url = require('url')
 exports.martha_v1 = (req, res) => {
   //allow browser to request this from another app
   if (req.method === `OPTIONS`) {
-    res.set('Access-Control-Allow-Origin', "*")
+    res.set("Access-Control-Allow-Origin", "*");
   }
     var orig_url = req.body.url;
     var parsed_url = url.parse(orig_url);
