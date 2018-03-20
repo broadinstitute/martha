236c04a8078ba1270b9b00701b24d2a3f8013eb8
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2018-03-20 13:30:20 -0400

    trying something else

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ exports.martha_v1 = (req, res) => {
   //allow browser to request this from broad sites
   if(req.headers && req.headers.hasOwnProperty('origin')) {
     console.log("setting header");
-    res.setHeader('Access-Control-Allow-Origin', "*broadinstitute\.org");
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader('Access-Control-Allow-Methods', ["POST", "GET"]);
     console.log("res");
     console.log(res);
   }
+  console.log(`req body: ${req.body}`);
   var orig_url = req.body["url"];
   var parsed_url = url.parse(orig_url);
   var orig_path = parsed_url.path;
