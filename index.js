c5a5efdd7785e0d6b7afe378e118c8b4d769bd82
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2018-03-21 14:02:39 -0400

    no idea what is happening!

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const superagent = require('superagent');
 const url = require('url');
 
 exports.martha_v1 = (req, res) => {
+  console.log(req.body);
   console.log(Type(req.body.url));
   console.log(req.body.url);
   var orig_url = req.body.url;
