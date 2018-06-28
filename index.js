00a654e6d316532329f9723f101560d5e84cd8e9
Douglas Voet <dvoet@users.noreply.github.com>
2018-06-27 20:06:27 -0400

    v2

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const superagent = require('superagent');
 const url = require('url')
 const cors = require("cors");
 const corsMiddleware = cors();
+const config = require("./config.json")
 
 function dosToHttps(dosUri) {
     var parsed_url = url.parse(dosUri);
@@ -16,7 +17,7 @@ function dosToHttps(dosUri) {
     // special case for hostless dos uris which will be better supported in martha v2
     if (parsed_url.host.startsWith("dg.")) {
         new_path = '/ga4gh/dos/v1/dataobjects/' + parsed_url.hostname + orig_path;
-        parsed_url.host = "dcp.bionimbus.org";
+        parsed_url.host = config.dosResolutionHost;
     }
 
     console.log(new_path);
@@ -28,7 +29,7 @@ function dosToHttps(dosUri) {
     return url.format(parsed_url);
 }
 
-const handler = (req, res) => {
+const martha_v1_handler = (req, res) => {
     var orig_url = req.body.url;
     var pattern = req.body.pattern;
     if(!orig_url) {
@@ -79,8 +80,54 @@ const handler = (req, res) => {
         });
 };
 
-exports.martha_v1 = (req, res) => {
-  corsMiddleware(req, res, () => handler(req, res));
+const martha_v2_handler = (req, res) => {
+    var orig_url = req.body.url;
+    var pattern = req.body.pattern;
+    if(!orig_url) {
+        orig_url = JSON.parse(req.body.toString()).url;
+    }
+
+    var http_url = dosToHttps(orig_url)
+
+    console.log(http_url);
+    superagent.get(http_url)
+        .end(function(err, response) {
+            if(err) {
+                console.error(err);
+                res.status(502).send(err);
+                return;
+            };
+            try {
+                var parsedData = JSON.parse(response.text);
+            } catch(e) {
+                res.status(400).send(`Data returned not in correct format`);
+                return;
+            };
+            superagent
+                .get(`${config.bondBaseUrl}/link/v1/fence/serviceaccount/key`)
+                .set('authorization', req.get("authorization"))
+                .end(function(bondErr, bondResponse) {
+                    if(bondErr) {
+                        console.error(err);
+                        res.status(502).send(err);
+                        return;
+                    };
+                    try {
+                        var parsedServiceAccountKey = JSON.parse(bondResponse.text);
+                    } catch(e) {
+                        res.status(500).send(`Service account key not in correct format`);
+                        return;
+                    };
+                    res.status(200).send({'dos': parsedData, 'credentials': parsedServiceAccountKey})
+                });
+        });
 };
 
-exports.dosToHttps = dosToHttps
+exports.martha_v1 = (req, res) => {
+  corsMiddleware(req, res, () => martha_v1_handler(req, res));
+};
+exports.martha_v2 = (req, res) => {
+    corsMiddleware(req, res, () => martha_v2_handler(req, res));
+};
+
+exports.dosToHttps = dosToHttps;
