6b148302b35a50f8916831ce8b8b80d8862c815e
Isaac Zarsky <zarsky@broadinstitute.org>
2018-07-23 17:16:14 -0400

    Cleanup code and use node 8 (#82)
    * cleanup deps
    
    * use node 8
    
    * js style cleanup

--- api_adapter.js
+++ api_adapter.js
@@ -1,13 +1,13 @@
-const request = require("superagent");
+const request = require('superagent');
 
 function getTextFrom(url, authorization = null) {
     const getReq = request.get(url);
     if (authorization != null) {
-        getReq.set("authorization", authorization);
+        getReq.set('authorization', authorization);
     }
     return getReq.then(function (response) {
-            return response.text;
-        });
+        return response.text;
+    });
 }
 
 exports.getTextFrom = getTextFrom;
--- index.js
+++ index.js
@@ -3,9 +3,9 @@
  * written by Ursa S 02/18
  */
 
-const corsMiddleware = require("cors")();
-const martha_v1 = require("./martha_v1");
-const martha_v2 = require("./martha_v2");
+const corsMiddleware = require('cors')();
+const martha_v1 = require('./martha_v1');
+const martha_v2 = require('./martha_v2');
 
 exports.martha_v1 = (req, res) => {
     corsMiddleware(req, res, () => martha_v1.martha_v1_handler(req, res));
