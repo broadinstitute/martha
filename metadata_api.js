5aca86da3d2af8f9db9d3d7e9bff43b6896cf1f6
Greg Polumbo <gpolumbo@broadinstitute.org>
2018-07-30 09:32:25 -0400

    style fixes/standardizations

--- metadata_api.js
+++ metadata_api.js
@@ -1,18 +1,6 @@
 const helpers = require('./helpers');
 const apiAdapter = require('./api_adapter');
 
-async function getMetadata(url, auth, isDos) {
-    try {
-        const uri = isDos ?
-            getGsUriFromDos((await getDosObject(url)).data_object) :
-            url;
-
-        return getGsObjectMetadata(uri, auth);
-    } catch (e) {
-        console.error('Metadata problems:', e);
-    }
-}
-
 function getRawMetadata(token, bucket, name) {
     return apiAdapter.getHeaders(
         `https://${bucket}.storage.googleapis.com/${encodeURIComponent(name)}`,
@@ -20,10 +8,10 @@ function getRawMetadata(token, bucket, name) {
     ).catch((e) => {
         switch (e.status) {
             case 403:
-                console.error("Permission denied for bucket object");
+                console.error('Permission denied for bucket object');
                 break;
             default:
-                console.error("Unexpected error while trying to get headers for bucket object")
+                console.error('Unexpected error while trying to get headers for bucket object')
         }
         throw e;
     });
@@ -35,7 +23,7 @@ function getPetTokenFromSam(bearerToken) {
         bearerToken,
         '["https://www.googleapis.com/auth/devstorage.full_control"]')
         .catch((e) => {
-            console.error("Failed to get Pet Service Account Token from Sam");
+            console.error('Failed to get Pet Service Account Token from Sam');
             throw e;
         });
 }
@@ -68,13 +56,25 @@ function parseGsUri(uri) {
 }
 
 function getGsUriFromDos(dosMetadata) {
-    return dosMetadata.urls.find(e => e.url.startsWith('gs://')).url;
+    return dosMetadata.urls.find((e) => e.url.startsWith('gs://')).url;
 }
 
 function getDosObject(dosUri) {
     const newUri = helpers.dosToHttps(dosUri);
 
-    return apiAdapter.getJsonFrom(newUri).catch((e) => console.error("Failed while trying to retrieve DOS object", e));
+    return apiAdapter.getJsonFrom(newUri).catch((e) => console.error('Failed while trying to retrieve DOS object', e));
+}
+
+async function getMetadata(url, auth, isDos) {
+    try {
+        const uri = isDos ?
+            getGsUriFromDos((await getDosObject(url)).data_object) :
+            url;
+
+        return getGsObjectMetadata(uri, auth);
+    } catch (e) {
+        console.error('Metadata problems:', e);
+    }
 }
 
 exports.getMetadata = getMetadata;
