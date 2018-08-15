d44aea5bc033207c955e0537e8017d64bc92034a
Greg Polumbo <gpolumbo@broadinstitute.org>
2018-08-15 10:50:31 -0400

    GAWB-3716 auth_tests loop over collection of dos urls

--- test/test_with_auth.test.js
+++ test/test_with_auth.test.js
@@ -26,10 +26,11 @@ function assertGoogleServiceAccount(response, t) {
 
 function handleResponse(err, response, t, skipGoogleServiceAccount = false) {
     if (err) {
+        let msg = err.message;
         if (response && response.text) {
-            console.error(response.text);
+            msg = msg + '\n' + response.text
         }
-        t.fail(err);
+        t.fail(msg);
     } else {
         assertDosObject(response, t);
         if (!skipGoogleServiceAccount) {
@@ -48,7 +49,7 @@ test.cb('martha_v2 responds with DOS object only when no "authorization" header 
         .end((err, response) => handleResponse(err, response, t, true));
 });
 
-test.cb('martha_v2 responds with DOS object when "authorization" header is provided', (t) => {
+test.failing.cb('martha_v2 responds with DOS object when "authorization" header is provided', (t) => {
     supertest
         .post('/martha_v2')
         .set('Content-Type', 'application/json')
@@ -58,18 +59,37 @@ test.cb('martha_v2 responds with DOS object when "authorization" header is provi
         .end((err, response) => handleResponse(err, response, t));
 });
 
-test.cb('martha_v2 responds with DOS object when "authorization" header is provided', (t) => {
-    const objectId = `dg.4503/00082e1c-42f6-4850-b512-413752286593`;
-    supertest
-        .post('/martha_v2')
-        .set('Content-Type', 'application/json')
-        .set('Authorization', `bearer ${bearerToken}`)
-        .send({ url: `dos://${objectId}` })
-        .expect(200)
-        .end((err, response) => handleResponse(err, response, t));
-});
+const dosUrls = [
+    'dos://dg.4503/00082e1c-42f6-4850-b512-413752286593',
+    'dos://qa.dcf.planx-pla.net/206dfaa6-bcf1-4bc9-b2d0-77179f0f48fc',
+    'dos://nci-crdc-staging.datacommons.io/206dfaa6-bcf1-4bc9-b2d0-77179f0f48fc',
+    'dos://dataguids.org/206dfaa6-bcf1-4bc9-b2d0-77179f0f48fc'
+];
 
-// https://qa.dcf.planx-pla.net/ga4gh/dos/v1/dataobjects/206dfaa6-bcf1-4bc9-b2d0-77179f0f48fc
+for (const dosUrl of dosUrls) {
+    test.cb(`Calling martha_v2 with URL: "${dosUrl}" without an Access Token resolves to a DOS object`, (t) => {
+        supertest
+            .post('/martha_v2')
+            .set('Content-Type', 'application/json')
+            .send({url: dosUrl})
+            .expect(200)
+            .end((err, response) => handleResponse(err, response, t, true));
+    });
+}
+
+// TODO: this slice should only be temporary until we can link accounts to the other DCF-Fence server
+const currentlyFailingUrls = dosUrls.slice(1, 4);
+for (const dosUrl of currentlyFailingUrls) {
+    test.failing.cb(`Calling martha_v2 with URL: "${dosUrl}" with an Access Token resolves to a DOS object and Google SA Key`, (t) => {
+        supertest
+            .post('/martha_v2')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `bearer ${bearerToken}`)
+            .send({ url: dosUrl })
+            .expect(200)
+            .end((err, response) => handleResponse(err, response, t));
+    });
+}
 
 // TODO: Test martha_v1 and fileSummaryV1
 // test
