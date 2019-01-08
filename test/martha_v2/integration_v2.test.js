b7451fdece4c71efe253eaf33544b9de298a333a
Marcus Talbott <mtalbott@broadinstitute.org>
2019-01-08 11:37:57 -0500

    use .pem instead of .json

--- test/martha_v2/integration_v2.test.js
+++ test/martha_v2/integration_v2.test.js
@@ -22,12 +22,12 @@ let fenceAuthLink = 'https://bond-fiab.dsde-dev.broadinstitute.org:31443/api/lin
 
 test.before(async () => {
     unauthorizedToken = await new GoogleToken({
-        keyFile: 'automation/firecloud-account.json',
+        keyFile: 'automation/firecloud-account.pem',
         sub: 'ron.weasley@test.firecloud.org',
         scope: 'profile email openid https://www.googleapis.com/auth/devstorage.full_control https://www.googleapis.com/auth/cloud-platform'
     }).getToken();
     authorizedToken = await new GoogleToken({
-        keyFile: 'automation/firecloud-account.json',
+        keyFile: 'automation/firecloud-account.pem',
         sub: 'hermione.owner@test.firecloud.org',
         scope: 'profile email openid https://www.googleapis.com/auth/devstorage.full_control https://www.googleapis.com/auth/cloud-platform'
     }).getToken();
