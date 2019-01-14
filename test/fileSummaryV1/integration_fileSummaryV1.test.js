11a12fde1689578d9e9537cd3b88f5b932bb36d5
Marc Talbott <mtalbott@broadinstitute.org>
2019-01-14 10:23:40 -0500

    Integration tests for Martha (#113)
    * port over gregs bond circleci changes, first effort
    
    * change working directory from /app to /martha
    
    * add base url to test command
    
    * change base url to local
    
    * remove trailing slash
    
    * trim base url
    
    * trim it even more
    
    * revert to url it supposedly should be
    
    * add push to quay
    
    * comment out smoketest script since it doesn't work and will be running different tests anyways
    
    * add integration tests; minor changes to circleci config to run these tests before pushing image to quay
    
    * change keyfile path to where i think it might be in fiab container
    
    * change to other keyfile path
    
    * use .pem instead of .json
    
    * add email to gtoken
    
    * address comments
    
    * clean up some issues according to codacy
    
    * add jira ticket to comment

---
+++ test/fileSummaryV1/integration_fileSummaryV1.test.js
@@ -0,0 +1,155 @@
+/** Run integration tests from the command line.  For example:
+ *
+ *    BASE_URL="https://us-central1-broad-dsde-dev.cloudfunctions.net" npm run-script integration_fileSummaryV1
+ *
+ * Run integration tests after a deployment to confirm that the functions deployed successfully
+ */
+
+const test = require('ava');
+const supertest = require('supertest')(process.env.BASE_URL);
+const assert = require('assert');
+const { GoogleToken } = require('gtoken');
+const { postJsonTo } = require('../../common/api_adapter');
+
+let unauthorizedToken;
+let authorizedToken;
+
+let keyFile = 'automation/firecloud-account.pem';
+let serviceAccountEmail = 'firecloud-dev@broad-dsde-dev.iam.gserviceaccount.com';
+let scopes = 'email openid';
+let unauthorizedEmail = 'ron.weasley@test.firecloud.org';
+let authorizedEmail = 'hermione.owner@test.firecloud.org';
+
+let dosUri = 'dos://dg.4503/preview_dos.json';
+let gsUri = 'gs://wb-mock-drs-dev/public/dos_test.txt';
+// TODO: GAWB-4053 -- remove static link so bond host can be changed depending on env
+let fenceAuthLink = 'https://bond-fiab.dsde-dev.broadinstitute.org:31443/api/link/v1/fence/oauthcode?oauthcode=IgnoredByMockProvider&redirect_uri=http%3A%2F%2Flocal.broadinstitute.org%2F%23fence-callback';
+
+test.before(async () => {
+    unauthorizedToken = await new GoogleToken({
+        keyFile: keyFile,
+        email: serviceAccountEmail,
+        sub: unauthorizedEmail,
+        scope: scopes
+    }).getToken();
+    authorizedToken = await new GoogleToken({
+        keyFile: keyFile,
+        email: serviceAccountEmail,
+        sub: authorizedEmail,
+        scope: scopes
+    }).getToken();
+
+    await postJsonTo(fenceAuthLink, 'Bearer ' + authorizedToken);
+});
+
+test.cb('integration_fileSummaryV1 responds with 400 if a uri is not provided', (t) => {
+    supertest
+        .post('/fileSummaryV1')
+        .set('Content-Type', 'application/json')
+        .send({ notValid: dosUri })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 400, 'Incorrect status code');
+            assert(response.text.includes('must specify the URI'), 'Received the wrong error message');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
+
+test.cb('integration_fileSummaryV1 responds with 400 if uri passed is malformed', (t) => {
+    supertest
+        .post('/fileSummaryV1')
+        .set('Content-Type', 'application/json')
+        .send({ uri: 'somethingNotValidURL' })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 400, 'Incorrect status code');
+            assert(response.text.includes('must specify the URI'), 'Received the wrong error message');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
+
+test.cb('integration_fileSummaryV1 responds with 401 if uri is valid but no authorization is provided', (t) => {
+    supertest
+        .post('/fileSummaryV1')
+        .set('Content-Type', 'application/json')
+        .send({ uri: dosUri })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 401, 'Incorrect status code');
+            assert(response.text.includes('must contain a bearer token'), 'Received the wrong error message');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
+
+test.cb('integration_fileSummaryV1 responds with 200 and file metadata but no signed url if dos uri is valid but the authorization header does not contain an authorized token', (t) => {
+    supertest
+        .post('/fileSummaryV1')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${unauthorizedToken}`)
+        .send({ uri: dosUri })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 200, 'Incorrect status code');
+            assert(response.body.bucket, 'fileSummary did not return metadata');
+            assert(!response.body.signedUrl, 'fileSummary returned a signed url but it should not have');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
+
+test.cb('integration_fileSummaryV1 responds with 200, file metadata, and a signed url if dos uri is valid and valid authorization is provided', (t) => {
+    supertest
+        .post('/fileSummaryV1')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${authorizedToken}`)
+        .send({ uri: dosUri })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 200, 'Incorrect status code');
+            assert(response.body.bucket, 'fileSummary did not return metadata');
+            assert(response.body.signedUrl, 'fileSummary did not return a signed url');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
+
+test.cb('integration_fileSummaryV1 responds with 502 and unauthorized response if gs uri is valid, but an invalid bearer token is provided', (t) => {
+    supertest
+        .post('/fileSummaryV1')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', 'Bearer badToken')
+        .send({ uri: gsUri })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 502, 'Incorrect status code');
+            assert.strictEqual(response.body.status, 401, 'Response should have been unauthorized');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
+
+test.cb('integration_fileSummaryV1 responds with 200, file metadata, and a signed url if gs uri is valid and valid authorization is provided', (t) => {
+    supertest
+        .post('/fileSummaryV1')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${authorizedToken}`)
+        .send({ uri: gsUri })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 200, 'Incorrect status code');
+            assert(response.body.bucket, 'fileSummary did not return metadata');
+            assert(response.body.signedUrl, 'fileSummary did not return a signed url');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
---
+++ test/martha_v2/integration_v2.test.js
@@ -0,0 +1,202 @@
+/** Run integration tests from the command line.  For example:
+ *
+ *    BASE_URL="https://us-central1-broad-dsde-dev.cloudfunctions.net" npm run-script integration_v2
+ *
+ * Run integration tests after a deployment to confirm that the functions deployed successfully
+ */
+
+const test = require('ava');
+const supertest = require('supertest')(process.env.BASE_URL);
+const assert = require('assert');
+const { GoogleToken } = require('gtoken');
+const { postJsonTo } = require('../../common/api_adapter');
+
+let unauthorizedToken;
+let authorizedToken;
+
+let keyFile = 'automation/firecloud-account.pem';
+let serviceAccountEmail = 'firecloud-dev@broad-dsde-dev.iam.gserviceaccount.com';
+let scopes = 'email openid';
+let unauthorizedEmail = 'ron.weasley@test.firecloud.org';
+let authorizedEmail = 'hermione.owner@test.firecloud.org';
+
+let publicFenceUrl = 'dos://dg.4503/preview_dos.json';
+let protectedFenceUrl = 'dos://dg.4503/65e4cd14-f549-4a7f-ad0c-d29212ff6e46';
+// TODO: remove static link so bond host can be changed depending on env
+let fenceAuthLink = 'https://bond-fiab.dsde-dev.broadinstitute.org:31443/api/link/v1/fence/oauthcode?oauthcode=IgnoredByMockProvider&redirect_uri=http%3A%2F%2Flocal.broadinstitute.org%2F%23fence-callback';
+
+
+test.before(async () => {
+    unauthorizedToken = await new GoogleToken({
+        keyFile: keyFile,
+        email: serviceAccountEmail,
+        sub: unauthorizedEmail,
+        scope: scopes
+    }).getToken();
+    authorizedToken = await new GoogleToken({
+        keyFile: keyFile,
+        email: serviceAccountEmail,
+        sub: authorizedEmail,
+        scope: scopes
+    }).getToken();
+
+    await postJsonTo(fenceAuthLink, 'Bearer ' + authorizedToken);
+});
+
+test.cb('integration_v2 responds with DOS object only when no "authorization" header is provided for a public url', (t) => {
+    supertest
+        .post('/martha_v2')
+        .set('Content-Type', 'application/json')
+        .send({ url: publicFenceUrl })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 200, 'Incorrect status code'); // not using loose equality for now, but if type coercion is wanted use equal instead of strictEqual
+            assert(response.body.dos, 'No DOS object found');
+            assert(!response.body.googleServiceAccount, 'Response should not have a Google Service Account');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
+
+test.cb('integration_v2 responds with DOS object and service account when "authorization" header is provided for a public url', (t) => {
+    supertest
+        .post('/martha_v2')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${authorizedToken}`)
+        .send({ url: publicFenceUrl })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 200, 'Incorrect status code');
+            assert(response.body.dos, 'No DOS object found');
+            assert(response.body.googleServiceAccount, 'No Google Service Account found');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
+
+test.cb('integration_v2 fails when "authorization" header is provided for a public url but user is not authed with provider', (t) => {
+    supertest
+        .post('/martha_v2')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${unauthorizedToken}`)
+        .send({ url: publicFenceUrl })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 502, 'Incorrect status code');
+            assert.strictEqual(response.body.status, 400, 'User should not be authorized with provider');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
+
+test.cb('integration_v2 fails when "authorization" header is provided for a public url but the bearer token is invalid', (t) => {
+    supertest
+        .post('/martha_v2')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer badToken`)
+        .send({ url: publicFenceUrl })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 502, 'Incorrect status code');
+            assert.strictEqual(response.body.status, 401, 'Bond should not have authenticated this token');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
+
+test.cb('integration_v2 responds with DOS object only when no "authorization" header is provided for a protected url', (t) => {
+    supertest
+        .post('/martha_v2')
+        .set('Content-Type', 'application/json')
+        .send({ url: protectedFenceUrl })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 200, 'Incorrect status code');
+            assert(response.body.dos, 'No DOS object found');
+            assert(!response.body.googleServiceAccount, 'Response should not have a Google Service Account');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
+
+test.cb('integration_v2 responds with DOS object and service account when "authorization" header is provided for a protected url', (t) => {
+    supertest
+        .post('/martha_v2')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${authorizedToken}`)
+        .send({ url: protectedFenceUrl })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 200, 'Incorrect status code');
+            assert(response.body.dos, 'No DOS object found');
+            assert(response.body.googleServiceAccount, 'No Google Service Account found');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
+
+test.cb('integration_v2 fails when "authorization" header is provided for a protected url but user is not authed with provider', (t) => {
+    supertest
+        .post('/martha_v2')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${unauthorizedToken}`)
+        .send({ url: protectedFenceUrl })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 502, 'Incorrect status code');
+            assert.strictEqual(response.body.status, 400, 'User should not be authorized with provider');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
+
+test.cb('integration_v2 fails when "authorization" header is provided for a protected url but the bearer token is invalid', (t) => {
+    supertest
+        .post('/martha_v2')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', 'Bearer badToken')
+        .send({ url: protectedFenceUrl })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 502, 'Incorrect status code');
+            assert.strictEqual(response.body.status, 401, 'Bond should not have authenticated this token');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
+
+test.cb('integration_v2 return error if url passed is malformed', (t) => {
+    supertest
+        .post('/martha_v2')
+        .set('Content-Type', 'application/json')
+        .send({ url: 'somethingNotValidURL' })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 400, 'Incorrect status code');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
+
+test.cb('integration_v2 return error if url passed is not good', (t) => {
+    supertest
+        .post('/martha_v2')
+        .set('Content-Type', 'application/json')
+        .send({ url: 'dos://broad-dsp-dos-TYPO.storage.googleapis.com/something-that-does-not-exist' })
+        .expect((response) => {
+            assert.strictEqual(response.statusCode, 502, 'Incorrect status code');
+        })
+        .end((error, response) => {
+            if (error) { t.log(response.body); }
+            t.end(error);
+        });
+});
