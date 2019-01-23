b516e299c30719e681c387d53b80c7f96383145c
Greg Polumbo <36171087+gpolumbo-broad@users.noreply.github.com>
2019-01-23 16:34:15 -0500

    GAWB-4009 (#115)
    GAWB-4009 tweaks to get the tests running in jenkins automation pipeline for indie services

--- test/martha_v2/integration_v2.test.js
+++ test/martha_v2/integration_v2.test.js
@@ -14,17 +14,19 @@ const { postJsonTo } = require('../../common/api_adapter');
 let unauthorizedToken;
 let authorizedToken;
 
+const myEnv = process.env.ENV ? process.env.ENV : 'dev';
+const emailDomain = (myEnv === 'qa' ? 'quality' : 'test') + '.firecloud.org';
+
 let keyFile = 'automation/firecloud-account.pem';
-let serviceAccountEmail = 'firecloud-dev@broad-dsde-dev.iam.gserviceaccount.com';
+const serviceAccountEmail = `firecloud-${myEnv}@broad-dsde-${myEnv}.iam.gserviceaccount.com`;
 let scopes = 'email openid';
-let unauthorizedEmail = 'ron.weasley@test.firecloud.org';
-let authorizedEmail = 'hermione.owner@test.firecloud.org';
+const unauthorizedEmail = `ron.weasley@${emailDomain}`;
+const authorizedEmail = `hermione.owner@${emailDomain}`;
 
 let publicFenceUrl = 'dos://dg.4503/preview_dos.json';
 let protectedFenceUrl = 'dos://dg.4503/65e4cd14-f549-4a7f-ad0c-d29212ff6e46';
 // TODO: remove static link so bond host can be changed depending on env
-let fenceAuthLink = 'https://bond-fiab.dsde-dev.broadinstitute.org:31443/api/link/v1/fence/oauthcode?oauthcode=IgnoredByMockProvider&redirect_uri=http%3A%2F%2Flocal.broadinstitute.org%2F%23fence-callback';
-
+const fenceAuthLink = `https://bond-fiab.dsde-${myEnv}.broadinstitute.org:31443/api/link/v1/fence/oauthcode?oauthcode=IgnoredByMockProvider&redirect_uri=http%3A%2F%2Flocal.broadinstitute.org%2F%23fence-callback`;
 
 test.before(async () => {
     unauthorizedToken = await new GoogleToken({
