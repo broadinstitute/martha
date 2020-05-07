a858e6dbed1a56e43555b1e40b452730474d4f2c
Rebecca Asch <rasch@broadinstitute.org>
2020-05-07 17:14:21 -0400

    removed unnecessary call using '@google-cloud/storage

--- handlers/getSignedUrlV1.js
+++ handlers/getSignedUrlV1.js
@@ -1,4 +1,3 @@
-const { Storage } = require('@google-cloud/storage');
 const apiAdapter = require('../common/api_adapter');
 const { promiseHandler, Response, samBaseUrl } = require('../common/helpers');
 const { bondBaseUrl, BondProviders, determineBondProvider } = require('../common/bond');
@@ -11,12 +10,7 @@ const getSignedUrlV1 = promiseHandler(async (req) => {
         const credentials = (provider && provider !== BondProviders.HCA) ?
             (await apiAdapter.getJsonFrom(`${bondBaseUrl()}/api/link/v1/${provider}/serviceaccount/key`, auth)).data :
             await apiAdapter.getJsonFrom(`${samBaseUrl()}/api/google/v1/user/petServiceAccount/key`, auth);
-        const storage = new Storage({ credentials });
-        const [url] = await storage.bucket(bucket).file(object).getSignedUrl({
-            version: 'v4',
-            action: 'read',
-            expires: Date.now() + 36e5
-        });
+        const url = urlSigner.createSignedGsUrl(credentials, { bucket, name });
         return new Response(200, { url });
     } catch (e) {
         throw provider && e.response ? new Response(e.status, JSON.parse(e.response.text).error) : e;
