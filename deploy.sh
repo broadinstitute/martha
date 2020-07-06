33eff80a9c243eb5c9ae2f3f17a82dc5e7ae002d
Khalid Shakir <kshakir@broadinstitute.org>
2020-07-05 22:33:26 -0400

    Added option to deploy to cromwell-dev

--- deploy.sh
+++ deploy.sh
@@ -1,63 +1,73 @@
 #!/usr/bin/env bash
-set -e
+set -eo pipefail
 set -x
 
-VAULT_TOKEN=$1
-GIT_BRANCH=$2
-TARGET_ENV=$3
+VAULT_TOKEN="$1"
+GIT_BRANCH="$2"
+DEPLOY_ENV="$3"
 
 set +x
-if [ -z "$TARGET_ENV" ]; then
-    echo "TARGET_ENV argument not supplied; inferring from GIT_BRANCH '$GIT_BRANCH'."
 
-    if [ "$GIT_BRANCH" == "dev" ]; then
-        TARGET_ENV="dev"
-    elif [ "$GIT_BRANCH" == "alpha" ]; then
-        TARGET_ENV="alpha"
-    elif [ "$GIT_BRANCH" == "perf" ]; then
-        TARGET_ENV="perf"
-    elif [ "$GIT_BRANCH" == "staging" ]; then
-        TARGET_ENV="staging"
-    elif [ "$GIT_BRANCH" == "master" ]; then
-        TARGET_ENV="prod"
+if [[ -z "${DEPLOY_ENV}" ]]; then
+    echo "DEPLOY_ENV argument not supplied; inferring from GIT_BRANCH '${GIT_BRANCH}'."
+
+    if [[ "${GIT_BRANCH}" == "dev" ]]; then
+        DEPLOY_ENV="dev"
+    elif [[ "${GIT_BRANCH}" == "alpha" ]]; then
+        DEPLOY_ENV="alpha"
+    elif [[ "${GIT_BRANCH}" == "perf" ]]; then
+        DEPLOY_ENV="perf"
+    elif [[ "${GIT_BRANCH}" == "staging" ]]; then
+        DEPLOY_ENV="staging"
+    elif [[ "${GIT_BRANCH}" == "master" ]]; then
+        DEPLOY_ENV="prod"
     else
-        echo "Git branch '$GIT_BRANCH' is not configured to automatically deploy to a target environment"
+        echo "Git branch '${GIT_BRANCH}' is not configured to automatically deploy to an environment"
         exit 1
     fi
 fi
 
-if [[ "$TARGET_ENV" =~ ^(dev|alpha|perf|staging|prod)$ ]]; then
-    ENVIRONMENT=${TARGET_ENV}
+set -u
+
+MARTHA_PATH=/martha
+SERVICE_ACCT_KEY_FILE="deploy_account.json"
+MARTHA_IMAGE="quay.io/broadinstitute/martha:${GIT_BRANCH}"
+DEPLOY_PROJECT_NAME="broad-dsde-${DEPLOY_ENV}"
+
+if [[ "${DEPLOY_ENV}" =~ ^(cromwell-dev)$ ]]; then
+    SOURCE_ENV="dev"
+elif [[ "${DEPLOY_ENV}" =~ ^(dev|alpha|perf|staging|prod)$ ]]; then
+    SOURCE_ENV="${DEPLOY_ENV}"
 else
-    echo "Unknown environment: $TARGET_ENV - must be one of [dev, alpha, perf, staging, prod]"
+    echo "Unknown environment: ${DEPLOY_ENV} - must be one of [cromwell-dev, dev, alpha, perf, staging, prod]"
     exit 1
 fi
 
-echo "Deploying branch '${GIT_BRANCH}' to ${ENVIRONMENT}"
+echo "Deploying branch '${GIT_BRANCH}' from ${SOURCE_ENV} to ${DEPLOY_PROJECT_NAME}"
 set -x
 
-PROJECT_NAME="broad-dsde-${ENVIRONMENT}"
-
-SERVICE_ACCT_KEY_FILE="deploy_account.json"
 # Get the tier specific credentials for the service account out of Vault
 # Put key into SERVICE_ACCT_KEY_FILE
-docker run --rm -e VAULT_TOKEN=${VAULT_TOKEN} broadinstitute/dsde-toolbox vault read --format=json "secret/dsde/martha/${ENVIRONMENT}/deploy-account.json" | jq .data > ${SERVICE_ACCT_KEY_FILE}
+docker run \
+    --rm \
+    --env "VAULT_TOKEN=${VAULT_TOKEN}" \
+    broadinstitute/dsde-toolbox \
+    vault read \
+    --format=json "secret/dsde/martha/${DEPLOY_ENV}/deploy-account.json" |
+jq .data > "${SERVICE_ACCT_KEY_FILE}"
 
-MARTHA_PATH=/martha
 # Process all Consul .ctmpl files
 # Vault token is required by the docker image regardless of whether you having any data in Vault or not
-docker run --rm -v $PWD:${MARTHA_PATH} \
-  -e INPUT_PATH=${MARTHA_PATH} \
-  -e OUT_PATH=${MARTHA_PATH} \
-  -e ENVIRONMENT=${ENVIRONMENT} \
-  -e VAULT_TOKEN=${VAULT_TOKEN} \
-  -e RUN_CONTEXT=live \
-  -e DNS_DOMAIN=NULL \
+docker run \
+  --rm \
+  --volume "$PWD:${MARTHA_PATH}" \
+  --env "INPUT_PATH=${MARTHA_PATH}" \
+  --env "OUT_PATH=${MARTHA_PATH}" \
+  --env "ENVIRONMENT=${SOURCE_ENV}" \
+  --env "VAULT_TOKEN=${VAULT_TOKEN}" \
+  --env "RUN_CONTEXT=live" \
+  --env "DNS_DOMAIN=NULL" \
   broadinstitute/dsde-toolbox render-templates.sh
-
-MARTHA_IMAGE=quay.io/broadinstitute/martha:${GIT_BRANCH}
-
-# Overriding ENTRYPOINT has some subtleties: https://medium.com/@oprearocks/how-to-properly-override-the-entrypoint-using-docker-run-2e081e5feb9d
 
 # > Note: As of January 15, 2020, HTTP functions require authentication by default.
 #
@@ -68,7 +78,7 @@ MARTHA_IMAGE=quay.io/broadinstitute/martha:${GIT_BRANCH}
 #   gcloud beta functions \
 #     add-iam-policy-binding {FUNCTION_NAME} \
 #     --member=allUsers --role=roles/cloudfunctions.invoker \
-#     --project=broad-dsde-{ENVIRONMENT}
+#     --project=broad-dsde-{DEPLOY_ENV}
 #   ```
 #
 # Until that function is run, `allUsers` will receive a permission denied error when invoking the new Google Cloud
@@ -76,20 +86,29 @@ MARTHA_IMAGE=quay.io/broadinstitute/martha:${GIT_BRANCH}
 #
 # After an `owner` fixes the permissions using the above `gcloud` command, the function contents may be overwritten by
 # any `editor`, including the Google Cloud Function service account stored in
-# `secret/dsde/martha/${ENVIRONMENT}/deploy-account.json`. The Google Cloud Function will stay public to `allUsers` even
+# `secret/dsde/martha/${SOURCE_ENV}/deploy-account.json`. The Google Cloud Function will stay public to `allUsers` even
 # when redeployed by an `editor`.
 
-docker run --rm \
-    --entrypoint="/bin/bash" \
-    -v $PWD:${MARTHA_PATH} \
-    -e BASE_URL="https://us-central1-broad-dsde-${ENVIRONMENT}.cloudfunctions.net" \
-    ${MARTHA_IMAGE} -c \
-    "gcloud config set project ${PROJECT_NAME} &&
-     gcloud auth activate-service-account --key-file ${MARTHA_PATH}/${SERVICE_ACCT_KEY_FILE} &&
-     cd ${MARTHA_PATH} &&
-     gcloud beta functions deploy martha_v2 --source=. --trigger-http --runtime nodejs8 &&
-     gcloud beta functions deploy martha_v3 --source=. --runtime nodejs8 --trigger-http &&
-     gcloud beta functions deploy fileSummaryV1 --source=. --runtime nodejs8 --trigger-http &&
-     gcloud beta functions deploy getSignedUrlV1 --source=. --runtime nodejs8 --trigger-http &&
-     npm ci &&
-     npm run-script smoketest"
+# Overriding ENTRYPOINT has some subtleties:
+# https://medium.com/@oprearocks/how-to-properly-override-the-entrypoint-using-docker-run-2e081e5feb9d
+
+docker run \
+    --rm \
+    --entrypoint="" \
+    --volume "$PWD:${MARTHA_PATH}" \
+    --env BASE_URL="https://us-central1-broad-dsde-${DEPLOY_ENV}.cloudfunctions.net" \
+    "${MARTHA_IMAGE}" \
+    /bin/bash -c \
+    "gcloud config set project ${DEPLOY_PROJECT_NAME} &&
+      gcloud auth activate-service-account --key-file ${MARTHA_PATH}/${SERVICE_ACCT_KEY_FILE} &&
+      cd ${MARTHA_PATH} &&
+      gcloud beta functions deploy martha_v2 --trigger-http --source=. --runtime nodejs8 \\
+        --allow-unauthenticated --project ${DEPLOY_PROJECT_NAME} &&
+      gcloud beta functions deploy martha_v3 --trigger-http --source=. --runtime nodejs8 \\
+        --allow-unauthenticated --project ${DEPLOY_PROJECT_NAME} &&
+      gcloud beta functions deploy fileSummaryV1 --trigger-http --source=. --runtime nodejs8 \\
+        --allow-unauthenticated --project ${DEPLOY_PROJECT_NAME} &&
+      gcloud beta functions deploy getSignedUrlV1 --trigger-http --source=. --runtime nodejs8 \\
+        --allow-unauthenticated --project ${DEPLOY_PROJECT_NAME} &&
+      npm ci &&
+      npm run-script smoketest"
