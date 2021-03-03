578a543097f01622de9e465c70b53bb018c1157e
Brian Reilly <breilly@broadinstitute.org>
2021-03-03 16:23:20 -0500

    Test runs and fails

--- test/martha/martha_v3.test.js
+++ test/martha/martha_v3.test.js
@@ -491,7 +491,7 @@ test.serial('martha_v3 parses BDC response correctly', async (t) => {
     getJsonFromApiStub.onCall(0).resolves(bdcDrsResponse);
     getJsonFromApiStub.onCall(1).resolves(bondAccessTokenResponse);
     getJsonFromApiStub.onCall(2).resolves(drsSignedUrlResponse);
-    getJsonFromApiStub.onCall(3).resolves(googleSAKeyObject)
+    getJsonFromApiStub.onCall(3).resolves(googleSAKeyObject);
 
     const response = mockResponse();
     await marthaV3(
@@ -502,7 +502,7 @@ test.serial('martha_v3 parses BDC response correctly', async (t) => {
     // Bond was called twice to get SA key and access token.
     // DRS server was called twice to get DRS response and signed URL.
     // 2 + 2 = 4.
-    t.true(getJsonFromApiStub.callCount(4));
+    t.is(getJsonFromApiStub.callCount, 4);
 
     t.deepEqual({ ...result }, bdcDrsMarthaResult(googleSAKeyObject, drsSignedUrlResponse.url));
     t.is(response.statusCode, 200);
@@ -514,19 +514,19 @@ test.serial('martha_v3 parses BDC response correctly', async (t) => {
     );
     t.falsy(getJsonFromApiStub.call(0).args[1]); // no auth passed
 
-    const requestedBondAccessToken = getJsonFromApiStub.call(1).args[0]
-    const accessTokenMatches = requestedBondAccessToken.match(bondAccessTokenRegEx)
-    t.truthy(accessTokenMatches, 'Bond access token URL called does not match Bond access token URL regular expression')
+    const requestedBondAccessToken = getJsonFromApiStub.call(1).args[0];
+    const accessTokenMatches = requestedBondAccessToken.match(bondAccessTokenRegEx);
+    t.truthy(accessTokenMatches, 'Bond access token URL called does not match Bond access token URL regular expression');
     const expectedAccessTokenProvider = 'fence';
     const actualAccessTokenProvider = accessTokenMatches[2];
     t.is(actualAccessTokenProvider, expectedAccessTokenProvider);
 
     t.is(
         getJsonFromApiStub.call(2).args[0],
-    'https://staging.gen3.biodatacatalyst.nhlbi.nih.gov/ga4gh/drs/v1/objects' +
+        'https://staging.gen3.biodatacatalyst.nhlbi.nih.gov/ga4gh/drs/v1/objects' +
         '/dg.4503/fc046e84-6cf9-43a3-99cc-ffa2964b88cb/access/gs',
     );
-    t.falsy(getJsonFromApiStub.call(2).args[1]); // no auth passed
+    t.is(getJsonFromApiStub.call(2).args[1], bondAccessTokenResponse.token);
 
     const requestedBondUrl = getJsonFromApiStub.call(3).args[0];
     const matches = requestedBondUrl.match(bondRegEx);
