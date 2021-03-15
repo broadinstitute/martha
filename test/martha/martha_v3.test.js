a629900535361a8a42393da9df765d7f2a489a27
Brian Reilly <breilly@broadinstitute.org>
2021-03-15 16:39:23 -0400

    fixed a test

--- test/martha/martha_v3.test.js
+++ test/martha/martha_v3.test.js
@@ -587,7 +587,7 @@ test.serial('martha_v3 parses Anvil response correctly', async (t) => {
     t.is(response.statusCode, 200);
 
     const result = response.send.lastCall.args[0];
-    sinon.assert.callCount(getJsonFromApiStub, 4) // Bond was called to get SA key
+    sinon.assert.callCount(getJsonFromApiStub, 4); // Bond was called to get SA key
     t.deepEqual({ ...result }, anvilDrsMarthaResult(googleSAKeyObject, drsSignedUrlResponse.url));
     t.is(
         getJsonFromApiStub.firstCall.args[0],
@@ -603,22 +603,26 @@ test.serial('martha_v3 parses Anvil response correctly', async (t) => {
 });
 
 test.serial('martha_v3 parses a The AnVIL CIB URI response correctly', async (t) => {
-    getJsonFromApiStub.onFirstCall().resolves(anvilDrsResponse);
+    getJsonFromApiStub.onCall(0).resolves(anvilDrsResponse);
+    getJsonFromApiStub.onCall(1).resolves(bondAccessTokenResponse);
+    getJsonFromApiStub.onCall(2).resolves(drsSignedUrlResponse);
+    getJsonFromApiStub.onCall(3).resolves(googleSAKeyObject);
     const response = mockResponse();
     await marthaV3(
         mockRequest({ body: { 'url': 'drs://dg.ANV0:dg.ANV0/00008531-03d7-418c-b3d3-b7b22b5381a0' } }),
         response,
     );
-    const result = response.send.lastCall.args[0];
-    t.true(getJsonFromApiStub.calledTwice); // Bond was called to get SA key
-    t.deepEqual({ ...result }, anvilDrsMarthaResult(googleSAKeyObject));
     t.is(response.statusCode, 200);
+
+    const result = response.send.lastCall.args[0];
+    sinon.assert.callCount(getJsonFromApiStub, 4); // Bond was called to get SA key
+    t.deepEqual({ ...result }, anvilDrsMarthaResult(googleSAKeyObject, drsSignedUrlResponse.url));
     t.is(
         getJsonFromApiStub.firstCall.args[0],
         `https://${config.HOST_THE_ANVIL_STAGING}/ga4gh/drs/v1/objects/dg.ANV0%2F00008531-03d7-418c-b3d3-b7b22b5381a0`,
     );
     t.falsy(getJsonFromApiStub.firstCall.args[1]); // no auth passed
-    const requestedBondUrl = getJsonFromApiStub.secondCall.args[0];
+    const requestedBondUrl = getJsonFromApiStub.getCall(3).args[0];
     const matches = requestedBondUrl.match(bondRegEx);
     t.truthy(matches, 'Bond URL called does not match Bond URL regular expression');
     const expectedProvider = 'anvil';
