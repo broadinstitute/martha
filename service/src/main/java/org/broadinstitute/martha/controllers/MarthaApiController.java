64e4af4e9c8ef63d5ab9c49fa4058ccabfc2c946
Kai <kfeldman@broadinstitute.org>
2022-02-02 17:02:08 -0500

    WIP port some stuff

--- service/src/main/java/org/broadinstitute/martha/controllers/MarthaApiController.java
+++ service/src/main/java/org/broadinstitute/martha/controllers/MarthaApiController.java
@@ -1,12 +1,15 @@
 package org.broadinstitute.martha.controllers;
 
+import bio.terra.common.exception.BadRequestException;
+import bio.terra.common.exception.UnauthorizedException;
+import java.util.ArrayList;
 import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.broadinstitute.martha.MarthaException;
 import org.broadinstitute.martha.generated.api.MarthaApi;
 import org.broadinstitute.martha.generated.model.RequestObject;
 import org.broadinstitute.martha.generated.model.ResourceMetadata;
+import org.broadinstitute.martha.models.Fields;
 import org.broadinstitute.martha.services.MetadataService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,25 +19,51 @@ import org.springframework.stereotype.Controller;
 public class MarthaApiController implements MarthaApi {
 
   private final HttpServletRequest request;
+  private final MetadataService metadataService;
 
-  public MarthaApiController(HttpServletRequest request) {
+  public MarthaApiController(HttpServletRequest request, MetadataService metadataService) {
     this.request = request;
+    this.metadataService = metadataService;
   }
 
   @Override
   public ResponseEntity<ResourceMetadata> getFile(RequestObject body) {
+    var auth = request.getHeader("authorization");
+
+    validateRequest(body, auth);
+
     var userAgent = request.getHeader("user-agent");
     var forceAccessUrl = Objects.equals(request.getHeader("martha-force-access-url"), "true");
     var ip = request.getHeader("X-Forwarded-For");
 
-    if (body == null || body.getUrl() == null) {
-      throw new MarthaException("Missing url in request body");
-    }
-
     log.info("Received URL '{}' from agent '{}' on IP '{}'", body.getUrl(), userAgent, ip);
 
-    new MetadataService(body.getUrl(), body.getFields(), null, forceAccessUrl).fetchResourceMetadata();
+    var resourceMetadata =
+        metadataService.fetchResourceMetadata(
+            body.getUrl(), body.getFields(), auth, forceAccessUrl);
 
-    return ResponseEntity.ok().build();
+    return ResponseEntity.ok(resourceMetadata);
+  }
+
+  private void validateRequest(RequestObject body, String auth) {
+    if (auth == null) {
+      throw new UnauthorizedException("Authorization header is missing.");
+    }
+    var errors = new ArrayList<String>();
+
+    if (body == null || body.getUrl() == null) {
+      errors.add("Missing url in request body");
+    }
+
+    if (body != null
+        && body.getFields() != null
+        && !Fields.ALL_FIELDS.containsAll(body.getFields())) {
+      errors.add(
+          String.format("Some fields were not valid. Supported fields are %s", Fields.ALL_FIELDS));
+    }
+
+    if (!errors.isEmpty()) {
+      throw new BadRequestException(String.join(",", errors));
+    }
   }
 }
--- service/src/main/java/org/broadinstitute/martha/services/MetadataService.java
+++ service/src/main/java/org/broadinstitute/martha/services/MetadataService.java
@@ -1,39 +1,41 @@
 package org.broadinstitute.martha.services;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
+import bio.terra.common.exception.BadRequestException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Objects;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import bio.terra.common.exception.BadRequestException;
 import org.broadinstitute.martha.config.MarthaConfig;
 import org.broadinstitute.martha.generated.model.ResourceMetadata;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class MetadataService {
+
   private final MarthaConfig marthaConfig;
+  private final Pattern compactIdRegex =
+      Pattern.compile(
+          "(?:dos|drs)://(?<host>dg\\.[0-9a-z-]+)(?<separator>:|/)(?<suffix>[^?]*)(?<query>\\?(.*))?",
+          Pattern.CASE_INSENSITIVE);
 
   public MetadataService(MarthaConfig marthaConfig) {
     this.marthaConfig = marthaConfig;
   }
 
-  public ResourceMetadata fetchResourceMetadata(String url, List<String> requestedFields, String auth, Boolean forceAccessUrl) {
+  public ResourceMetadata fetchResourceMetadata(
+      String url, List<String> requestedFields, String auth, Boolean forceAccessUrl) {
 
     return null;
   }
 
-  private UrlParts getHttpsUrlParts(String url) {
+  private UriComponents getHttpsUrlParts(String url) {
     /*
     DOS or DRS schemes are allowed as of AZUL-702
     https://ucsc-cgl.atlassian.net/browse/AZUL-702
-     */
 
-    /*
     The many, many forms of Compact Identifier-based (CIB) DRS URIs to W3C/IETF HTTPS URL conversion:
     - https://ga4gh.github.io/data-repository-service-schemas/preview/release/drs-1.1.0/docs/#_compact_identifier_based_drs_uris
     - https://docs.google.com/document/d/1Wf4enSGOEXD5_AE-uzLoYqjIp5MnePbZ6kYTVFp1WoM/edit
@@ -53,67 +55,25 @@ public class MetadataService {
     If you update *any* of the below be sure to link to the supporting docs and update the comments above!
      */
 
-    // The many different ways a DOS/DRS may be "compact", in the order that the should be tried
-//    var cibRegExps = List.of(
-//        // Non-W3C CIB DOS/DRS URIs, where the `dg.abcd` appears more than once
-//        // ex. drs://dg.1234:dg.1234/object_id?query_string
-//        Pattern.compile("(?:dos|drs)://(?<host>dg\\.[0-9a-z-]+)(?<separator>:)\\k<host>/(?<suffix>[^?]*)(?<query>\\?(.*))?", Pattern.CASE_INSENSITIVE),
-//        // Non-W3C CIB DOS/DRS URIs, where the `dg.abcd` is only mentioned once
-//        // ex. drs://dg.1234:object_id?query_string
-//        Pattern.compile("(?:dos|drs)://(?<host>dg\\.[0-9a-z-]+)(?<separator>:)(?<suffix>[^?]*)(?<query>\\?(.*))?", Pattern.CASE_INSENSITIVE),
-//        // W3C compatible using a slash separator
-//        // ex. drs://dg.1234/object_id?query_string
-//        Pattern.compile("(?:dos|drs)://(?<host>dg\\.[0-9a-z-]+)(?<separator>/)(?<suffix>[^?]*)(?<query>\\?(.*))?", Pattern.CASE_INSENSITIVE)
-//    );
+    var compactIdMatch = compactIdRegex.matcher(url);
 
-    var cibRegExp = Pattern.compile("(?:dos|drs)://(?<host>dg\\.[0-9a-z-]+)(?<separator>:|/)(?<suffix>[^?]*)(?<query>\\?(.*))?", Pattern.CASE_INSENSITIVE);
+    if (compactIdMatch.matches()) {
+      String cibHost = compactIdMatch.group("host");
 
-
-    //var cibRegExp = cibRegExps.stream().filter(pattern -> pattern.matcher(url).matches()).findFirst();
-
-    var cibMatch = cibRegExp.matcher(url);
-
-    if (cibMatch.matches()) {
-      String cibHost = cibMatch.group("host");
-      return new UrlParts(
-          Objects.requireNonNull(marthaConfig.getHosts().get(cibHost), String.format("Unrecognized Compact Identifier Based host [%s]", cibHost)),
-          URLEncoder.encode(cibMatch.group("suffix"), StandardCharsets.UTF_8),
-          cibMatch.group("query")
-        );
+      return UriComponentsBuilder.newInstance()
+          .host(
+              Objects.requireNonNull(
+                  marthaConfig.getHosts().get(cibHost),
+                  String.format("Unrecognized Compact Identifier Based host [%s]", cibHost)))
+          .path(URLEncoder.encode(compactIdMatch.group("suffix"), StandardCharsets.UTF_8))
+          .query(compactIdMatch.group("query"))
+          .build();
     } else {
-      // TODO: someone check my logic from here down (we ran out of time for mobbing)
-      URL parsedUrl;
-      try {
-        parsedUrl = new URL(url);
-      } catch (MalformedURLException e) {
-        throw new BadRequestException(e.getMessage());
-      }
-      if (parsedUrl.getHost().isEmpty() || parsedUrl.getPath().isEmpty()) {
+      var parsedUri = UriComponentsBuilder.fromUriString(url).build();
+      if (parsedUri.getHost() == null || parsedUri.getPath() == null) {
         throw new BadRequestException(String.format("[%s] is missing a host and/or a path.", url));
       }
-      // TODO: update this return value
-      //  (in doing so we may need to add an optional "port" value to the UrlParts class)
-      return new UrlParts(parsedUrl.getHost(), "", "");
-    }
-
-//    return {
-//        httpsUrlHost: parsedUrl.hostname,
-//        httpsUrlPort: parsedUrl.port,
-//        protocolSuffix: parsedUrl.pathname.slice(1),
-//        httpsUrlSearch: parsedUrl.search,
-//    };
-  }
-
-
-  class UrlParts {
-    String httpsUrlHost;
-    String protocolSuffix;
-    String httpsUrlSearch;
-
-    public UrlParts(String httpsUrlHost, String protocolSuffix, String httpsUrlSearch) {
-      this.httpsUrlHost = httpsUrlHost;
-      this.protocolSuffix = protocolSuffix;
-      this.httpsUrlSearch = httpsUrlSearch;
+      return parsedUri;
     }
   }
 }
