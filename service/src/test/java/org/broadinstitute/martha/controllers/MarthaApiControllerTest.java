b49101c8481c341f516ee656f7bc51aea3dc39c5
Kai <kfeldman@broadinstitute.org>
2022-02-09 10:42:09 -0500

    wip add test

--- service/src/test/java/org/broadinstitute/martha/controllers/MarthaApiControllerTest.java
+++ service/src/test/java/org/broadinstitute/martha/controllers/MarthaApiControllerTest.java
@@ -1,21 +1,22 @@
 package org.broadinstitute.martha.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.broadinstitute.martha.BaseTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.ContentResultMatchers;
 
 @AutoConfigureMockMvc
 public class MarthaApiControllerTest extends BaseTest {
 
   @Autowired private MockMvc mvc;
 
+  // TODO I think we need this as a mock bean but it doesn't exist yet
+  // @MockBean private ApiAdapter apiAdapterMock;
 
   // martha_v3 doesn't fail when extra data submitted besides a 'url'
   @Test
@@ -28,49 +29,50 @@ public class MarthaApiControllerTest extends BaseTest {
         .andExpect(status().isOk());
   }
 
-  //martha_v3 calls no endpoints when no fields are requested
+  // martha_v3 calls no endpoints when no fields are requested
   @Test
   void testMarthaCallsNoEndpointsWithNoFieldsRequested() throws Exception {
     var body = "{ url: `dos://${bdc}/123`, fields: [] }";
     // TODO: should the second be some kind of isEmpty instead of looking for the empty object?
     mvc.perform(post("/api/v4", body)).andExpect(status().isOk()).andExpect(content().json("{}"));
+    // verify(apiAdapterMock, never());
   }
 
-//  test.serial('martha_v3 calls no endpoints when no fields are requested', async (t) => {
-//    const response = mockResponse();
-//
-//    await marthaV3(mockRequest({
-//        body: {
-//      url: `dos://${bdc}/123`,
-//      fields: [],
-//    }
-//    }), response);
-//
-//    t.is(response.statusCode, 200);
-//    t.deepEqual(response.body, {});
-//
-//    sinon.assert.callCount(getJsonFromApiStub, 0);
-//  });
+  //  test.serial('martha_v3 calls no endpoints when no fields are requested', async (t) => {
+  //    const response = mockResponse();
+  //
+  //    await marthaV3(mockRequest({
+  //        body: {
+  //      url: `dos://${bdc}/123`,
+  //      fields: [],
+  //    }
+  //    }), response);
+  //
+  //    t.is(response.statusCode, 200);
+  //    t.deepEqual(response.body, {});
+  //
+  //    sinon.assert.callCount(getJsonFromApiStub, 0);
+  //  });
 
   //  const mockResponse = () => {
-//    return {
-//        status(s) {
-//        this.statusCode = s;
-//    return this;
-//        },
-//    send: sinon.mock('send').once().callsFake(function setBody(body) {
-//      // Express will effectively JSON.stringify objects passed to send, which is perfect for
-//      // sending over the wire and deserializing into a simple object on the other end.
-//      // We want a similar effect here, where all we get are the object properties (and
-//      // specifically no class instance details) so that we can make comparisons against
-//      // simple objects. Specifically, this takes care of cases where we "send" a
-//      // FailureResponse.
-//      this.body = { ...body };
-//      return this;
-//    }),
-//    setHeader: sinon.stub()
-//    };
-//  };
+  //    return {
+  //        status(s) {
+  //        this.statusCode = s;
+  //    return this;
+  //        },
+  //    send: sinon.mock('send').once().callsFake(function setBody(body) {
+  //      // Express will effectively JSON.stringify objects passed to send, which is perfect for
+  //      // sending over the wire and deserializing into a simple object on the other end.
+  //      // We want a similar effect here, where all we get are the object properties (and
+  //      // specifically no class instance details) so that we can make comparisons against
+  //      // simple objects. Specifically, this takes care of cases where we "send" a
+  //      // FailureResponse.
+  //      this.body = { ...body };
+  //      return this;
+  //    }),
+  //    setHeader: sinon.stub()
+  //    };
+  //  };
 
   // example old martha post request setup
   //  const mockRequest = (req, options = {}) => {
